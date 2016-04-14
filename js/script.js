f24f637bc4f5e2533ed6861f7127b159a89d50c1
David-Bohl <davebohl@gmail.com>
2016-04-14 16:09:49 -0400

    orb version

--- js/script.js
+++ js/script.js
@@ -12,15 +12,61 @@ $( document ).ready(function() {
   //   $(activeSection).find('h2').addClass('active');
   // });
 
+
+
+  
+  //change glow with distance
+  //calculate distance
+  var mX, mY, distance,$element  = $('#orb');
+
+  function calculateDistance(elem, mouseX, mouseY) {
+    return Math.floor(Math.sqrt(Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)));
+  }
+
+  //mousemove
+  $(document).mousemove(function(e) {
+    distance = calculateDistance($element, e.pageX, e.pageY);
+    var glowAmount = ( 1-(distance/500) );
+
+    // for orb
+    $("#orb").css({
+      'box-shadow' : "0 0 60px 30px rgba(255, 255, 255,"+glowAmount+"), 0 0 100px 60px rgba(255, 0, 255,"+glowAmount+"),0 0 140px 90px rgba(0, 255, 255,"+glowAmount+")"
+      ,'background-color' : "rgba(255, 255, 255,"+glowAmount*1.5+")"
+    });    
+
+    // for marble
+    // $("#marble").css({
+    //   'box-shadow' : "0 0 60px 30px rgba(255, 255, 255,"+glowAmount+"), 0 0 100px 60px rgba(255, 0, 255,"+glowAmount+"),0 0 140px 90px rgba(0, 255, 255,"+glowAmount+")"
+    //   ,'background-color' : "rgba(255, 255, 255,"+glowAmount*1.5+")"
+    // }); 
+
+  });// end mousemove
+
+
+
+
+  
+
+
+
+
+
+  // mouse position
+  //$( document ).on( "mousemove", function( event ) {
+    //console.log("x:" + event.pageX + "y:" + event.pageY);
+
+  //});
+
+
+
+
   $(window).scroll(function() {     
     var scroll = $(window).scrollTop();
     
-    //console.log(scroll);
-
-    var sunHeight = parseInt(300+scroll);
-    //console.log(sunHeight);
-
-    $('#sun').css({top:sunHeight});
+    //sunset
+    // var sunHeight = parseInt(300+scroll);
+    // console.log(sunHeight);
+    // $('#sun').css({top:sunHeight});
 
     //light up headers when active
     if( $('a[href="#about"]').parents("li").hasClass("active") )
@@ -41,12 +87,13 @@ $( document ).ready(function() {
       $('#contact-header').removeClass('active');
 
 
-    //change background color on scroll
+    //change background color on scroll--------------------
 
+    //mapping code
+    //$("header.intro,section#about,section#code,section#music,section#contact").css('background', 'rgb('+scroll.map(0,docHeight, 50,255 )+','+scroll.map(0,docHeight, 76,255 )+','+scroll.map(0,docHeight, 22,255 )+')');
 
-    // $("header.intro,section#about,section#code,section#music,section#contact").css('background', 'rgb('+scroll.map(0,docHeight, 50,255 )+','+scroll.map(0,docHeight, 76,255 )+','+scroll.map(0,docHeight, 22,255 )+')');
-    $("header.intro,section#about,section#code,section#music,section#contact").css('background', 'rgb('+ parseInt((1270-scroll*2)/10)+','+parseInt((1270-scroll*2)/10)+','+parseInt((2550-scroll*4)/10)+')');
-
+    //quick fade blue to black
+    //$("header.intro,section#about,section#code,section#music,section#contact").css('background', 'rgb('+ parseInt((1270-scroll*2)/10)+','+parseInt((1270-scroll*2)/10)+','+parseInt((2550-scroll*4)/10)+')');
 
     //original colors
       //$("header.intro,section#about,section#code,section#music,section#contact").css('background', 'rgb('+ parseInt(scroll/10)+','+parseInt(scroll/10)+','+parseInt(scroll/5)+')');
