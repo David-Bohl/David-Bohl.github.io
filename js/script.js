1fb6f6b87ad7fffae12a1b3ad84ad4e84c31230b
David-Bohl <davebohl@gmail.com>
2016-04-25 14:56:40 -0400

    sunset version

--- js/script.js
+++ js/script.js
@@ -86,18 +86,19 @@ $( document ).ready(function() {
     else
       $('#contact-header').removeClass('active');
 
-
     //change background color on scroll--------------------
 
     //mapping code
     //$("header.intro,section#about,section#code,section#music,section#contact").css('background', 'rgb('+scroll.map(0,docHeight, 50,255 )+','+scroll.map(0,docHeight, 76,255 )+','+scroll.map(0,docHeight, 22,255 )+')');
 
     //quick fade blue to black
-    //$("header.intro,section#about,section#code,section#music,section#contact").css('background', 'rgb('+ parseInt((1270-scroll*2)/10)+','+parseInt((1270-scroll*2)/10)+','+parseInt((2550-scroll*4)/10)+')');
+    $("header.intro,section#about,section#code,section#music,section#contact").css('background', 'rgb('+ parseInt((1270-scroll*2)/10)+','+parseInt((1270-scroll*2)/10)+','+parseInt((2550-scroll*4)/10)+')');
 
     //original colors
       //$("header.intro,section#about,section#code,section#music,section#contact").css('background', 'rgb('+ parseInt(scroll/10)+','+parseInt(scroll/10)+','+parseInt(scroll/5)+')');
 
+    //marble fade
+    //$("#marble").css("opacity", (1- (scroll/400) ));
 
   }); //end scroll
 
